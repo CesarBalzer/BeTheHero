d9eb1d464f238fa9c93620be7a017bc5f5fee3be
Cesar Balzer <cesarbalzer@Balzer-iMac.local>
2020-03-26 14:51:42 -0300

    Fix

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -13,20 +13,12 @@ import logoImg from '../../assets/logo.png';
 export default function Detail() {
 
     const navigation = useNavigation();
-    const message = `Olá ${incident.name},
-     estou entrando em contato, pois gostaria de ajudar no 
-     caso ${incident.title} com o valor de
-     ${Intl.NumberFormat(
-        'pt-BR',
-        {
-            style: 'currency',
-            currency: 'BRL'
-        }
-    ).format(incident.value)}`;
 
     const route = useRoute();
 
     const incident = route.params.incident;
+    
+    const message = `Olá ${incident.name}, estou entrando em contato, pois gostaria de ajudar no caso ${incident.title} com o valor de ${Intl.NumberFormat('pt-BR',{style: 'currency',currency: 'BRL'}).format(incident.value)}`;
 
     function navigateBack() {
         navigation.goBack();
